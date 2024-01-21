7565283093f54a141bf46bc056c72473eddbe875
Charles <charlesthiagosantos@gmail.com>
2024-01-21 09:56:50 -0300

    feat : criação do toStrig usando StringBuilder para retornar apenas vetores preenchidos

--- src/com/estruturadados/vetor/Vetor.java
+++ src/com/estruturadados/vetor/Vetor.java
@@ -1,22 +1,43 @@
 package com.estruturadados.vetor;
 
+import java.util.Arrays;
+
 public class Vetor {
     private String[] elementos;
+    private int tamanho;
 
     public Vetor(int capacidade) {
         this.elementos = new String[capacidade];
     }
 
-    public void adicionarElemento(String elemento) {
-        for(int i = 0; i < this.elementos.length; i ++) {
+    public int getTamanho() {
+        return tamanho;
+    }
 
-            if(elementos[i] == null) {
-                elementos[i] = elemento;
-                break; // Parar a aplicação quando encontrar uma posição nula
-            } else {
-                throw new IllegalArgumentException("Vetor preenchido");
-            }
+    public void adicionarElemento(String elemento) throws Exception {
+        if(tamanho < this.elementos.length) {
+            this.elementos[tamanho] = elemento;
+            tamanho ++;
+        } else {
+            throw new Exception("Ex: capacidade total do vetor preenchida!");
         }
     }
 
+    @Override
+    public String toString() {
+        StringBuilder string = new StringBuilder();
+        string.append("[");
+
+        for(int i = 0; i <tamanho -1; i++) {
+            string.append(elementos[i]);
+            string.append(", ");
+        }
+
+        if(elementos.length> 0) {
+            string.append(elementos[tamanho -1]);
+            string.append("]");
+        }
+
+        return string.toString() ;
+    }
 }
--- src/com/estruturadados/vetor/testa/Main.java
+++ src/com/estruturadados/vetor/testa/Main.java
@@ -4,10 +4,18 @@ import com.estruturadados.vetor.Vetor;
 
 public class Main {
     public static void main(String[] args) {
-        Vetor vetor = new Vetor(5);
+        Vetor vetor = new Vetor(10);
 
-        vetor.adicionarElemento("elemento 01");
+        try {
+            vetor.adicionarElemento("elemento 01");
+            vetor.adicionarElemento("elemento 02");
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
 
+        System.out.println(vetor.getTamanho());
+
+        System.out.println(vetor);
 
 
     }
