a1e31e239770266555d84c1eaed71b83b18a7648
Charles <charlesthiagosantos@gmail.com>
2024-03-29 16:23:09 -0300

    feat: criação da classe NoAluno

---
+++ src/com/estruturadados/listaEncadeada/pratica/NoAluno.java
@@ -0,0 +1,34 @@
+package com.estruturadados.listaEncadeada.pratica;
+
+public class NoAluno {
+    private Aluno aluno;
+    private NoAluno proximo;
+
+    public NoAluno(Aluno aluno) {
+        this.aluno = aluno;
+    }
+
+    public Aluno getAluno() {
+        return aluno;
+    }
+
+    public void setAluno(Aluno aluno) {
+        this.aluno = aluno;
+    }
+
+    public NoAluno getProximo() {
+        return proximo;
+    }
+
+    public void setProximo(NoAluno proximo) {
+        this.proximo = proximo;
+    }
+
+    @Override
+    public String toString() {
+        return "NoAluno{" +
+                "aluno=" + aluno +
+                ", proximo=" + proximo +
+                '}';
+    }
+}
